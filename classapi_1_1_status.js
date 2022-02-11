b75e286b55948dbb31b6e73f28f22bbc3b25bf82
hyoklee <hyoklee@users.noreply.github.com>
2022-02-11 03:42:16 +0000

    deploy: b21ddde4336adb817101abe6f571732438ab8539

---
+++ classapi_1_1_status.js
@@ -0,0 +1,12 @@
+var classapi_1_1_status =
+[
+    [ "Status", "classapi_1_1_status.html#a97b186faa08bf3f5efcd1fc38f5299b2", null ],
+    [ "Acceptable", "classapi_1_1_status.html#a614badfd271231991ed2e7664b09031a", null ],
+    [ "Failed", "classapi_1_1_status.html#a76bfec0bbcd53e1c3904e0d619021162", null ],
+    [ "GetStatus", "classapi_1_1_status.html#a5186760868ff020da32e27866ab0f7cb", null ],
+    [ "Msg", "classapi_1_1_status.html#ac896de1ec4175376237ca1088e901f23", null ],
+    [ "operator=", "classapi_1_1_status.html#aa07ab2da93eec3ff49e84775f413ba8b", null ],
+    [ "operator==", "classapi_1_1_status.html#af5c3b33dedea2851917d928a08fc1f21", null ],
+    [ "Succeeded", "classapi_1_1_status.html#a12ac0a86f097a9c62a01fca4dbed7e55", null ],
+    [ "error", "classapi_1_1_status.html#a71056da110a68114fef2cb08c9b44bf4", null ]
+];
---
+++ navtreedata.js
@@ -0,0 +1,66 @@
+/*
+@licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2019 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of version 2 of the GNU General Public License as published by
+the Free Software Foundation
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Hermes", "index.html", [
+    [ "Welcome to Hermes!", "index.html", [
+      [ "Important Coordinates", "index.html#sec_coordinates", null ]
+    ] ],
+    [ "Todo List", "todo.html", null ],
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html",
+"classhermes_1_1adapter_1_1stdio_1_1_balanced_mapper.html#af53ddd972ba1848c27f005af0079cd07",
+"memory__management_8h.html#a60c1e09296fea0acd1dc8c2e387f455f",
+"structhermes_1_1_id_list.html#af0e6cd8d116133427a07bafead927e57",
+"structhermes_1_1adapter_1_1stdio_1_1_file_struct.html#a32c14c00a511f7e22ee1a5c1d0d78289"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
