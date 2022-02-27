e313f8121b385187db9b5d30619a766dae745399
Hyo-Kyung Lee <hyoklee@hdfgroup.org>
2022-02-26 19:13:49 -0600

    Fix quote.

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -27,7 +27,7 @@ bash autogen.sh
 mkdir -p build
 pushd build
 CXXFLAGS=-I"${INSTALL_DIR}/include" LDFLAGS="-L${INSTALL_DIR}/lib -Wl,-rpath,${INSTALL_DIR}/lib" \
-        ../configure --prefix="${INSTALL_DIR} --enable-parallel "
+        ../configure --prefix="${INSTALL_DIR}" --enable-parallel
 make -j 4 && make install
 popd
 popd
