3987210fbd90309cfea81feecda801c9450a63dd
H. Joe Lee <hyoklee@hdfgroup.org>
2022-03-02 04:30:02 +0000

    Add IOR test script.

---
+++ ci/test_ior_mpi.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+HEREMS_CONF_PATH=/home/hermes/hermes/test/data/hermes.conf
+HERMES_INSTAA_DIR=/home/hermes/install
+CHECKPOINT_FILE=checkpoint.tmp
+mpirun -n 1 -ppn 1 \
+  -genv HERMES_CONF ${HERMES_CONF_PATH} \
+  ${HERMES_INSTALL_DIR}/bin/hermes_daemon &
+sleep 3
+mpirun -n 1 -ppn 1 \
+  -genv LD_PRELOAD ${HERMES_INSTALL_DIR}/lib/libhermes_mpiio.so \
+  -genv HERMES_CONF ${HERMES_CONF_PATH} \
+  -genv HERMES_CLIENT 1 \
+  -genv ADAPTER_MODE SCRATCH \
+  -genv HERMES_STOP_DAEMON 0 \
+  ior -a=MPIIO -w -k -o ${CHECKPOINT_FILE} -t 1m -b 128m -F -e -Y -O summaryFormat=CSV
+mpirun -n 1 -ppn 1 \
+  -genv LD_PRELOAD ${HERMES_INSTALL_DIR}/lib/libhermes_mpiio.so \
+  -genv HERMES_CONF ${HERMES_CONF_PATH} \
+  -genv HERMES_CLIENT 1 \
+  -genv ADAPTER_MODE SCRATCH \
+  ior -a=MPIIO -r -o ${CHECKPOINT_FILE} -t 1m -b 128m -F -e -O summaryFormat=CSV
