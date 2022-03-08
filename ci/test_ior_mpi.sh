0b12668448f13d3c0522c08f7b933e7309a8bafe
H. Joe Lee <hyoklee@hdfgroup.org>
2022-03-08 10:56:13 -0600

    Adjust -n param for MPIIO test.

--- ci/test_ior_mpi.sh
+++ ci/test_ior_mpi.sh
@@ -6,16 +6,16 @@ mpirun -n 1 -ppn 1 \
   -genv HERMES_CONF ${HERMES_CONF_PATH} \
   ${HERMES_INSTALL_DIR}/bin/hermes_daemon &
 sleep 3
-mpirun -n 1 -ppn 1 \
-  -genv LD_PRELOAD ${HERMES_INSTALL_DIR}/lib/libhermes_mpiio.so \
-  -genv HERMES_CONF ${HERMES_CONF_PATH} \
-  -genv HERMES_CLIENT 1 \
-  -genv ADAPTER_MODE SCRATCH \
-  -genv HERMES_STOP_DAEMON 0 \
-  ior -a=MPIIO -w -k -o ${CHECKPOINT_FILE} -t 1m -b 128m -F -e -Y -O summaryFormat=CSV
-# mpirun -n 1 -ppn 1 \
-#    -genv LD_PRELOAD ${HERMES_INSTALL_DIR}/lib/libhermes_mpiio.so \
-#    -genv HERMES_CONF ${HERMES_CONF_PATH} \
-#    -genv HERMES_CLIENT 1 \
-#    -genv ADAPTER_MODE SCRATCH \
-#   ior -a=MPIIO -r -o ${CHECKPOINT_FILE} -t 1m -b 128m -F -e -O summaryFormat=CSV
+# mpirun -n 2 -ppn 1 \
+#   -genv LD_PRELOAD ${HERMES_INSTALL_DIR}/lib/libhermes_mpiio.so \
+#   -genv HERMES_CONF ${HERMES_CONF_PATH} \
+#   -genv HERMES_CLIENT 1 \
+#   -genv ADAPTER_MODE SCRATCH \
+#   -genv HERMES_STOP_DAEMON 0 \
+#   ior -a=MPIIO -w -k -o ${CHECKPOINT_FILE} -t 1m -b 128m -F -e -Y -O summaryFormat=CSV
+mpirun -n 2 -ppn 1 \
+    -genv LD_PRELOAD ${HERMES_INSTALL_DIR}/lib/libhermes_mpiio.so \
+    -genv HERMES_CONF ${HERMES_CONF_PATH} \
+    -genv HERMES_CLIENT 1 \
+    -genv ADAPTER_MODE SCRATCH \
+   ior -a=MPIIO -r -o ${CHECKPOINT_FILE} -t 1m -b 128m -F -e -O summaryFormat=CSV
